ce9f128439c2a71e4e758a2cab1fda183716df32
Jacek <rysiuziom1@gmail.com>
2020-10-27 10:58:51 +0100

    change Home component to functional

--- trello/src/Home.js
+++ trello/src/Home.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    if (!this.props.isLoggedIn) {
-      return (
-        <div>
-          <Link to="/login">Log In</Link>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h2>You are successful logged</h2>
-          <br />
-          <br />
-          <button onClick={this.props.handleLogout}>Log Out</button>
-        </div>
-      );
-    }
+function Home(props) {
+  if (!props.isLoggedIn) {
+    return (
+      <div>
+        <Link to="/login">Log In</Link>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>You are successfully logged</h2>
+        <br />
+        <br />
+        <button onClick={props.handleLogout}>Log Out</button>
+      </div>
+    );
   }
 }
 
