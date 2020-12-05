066b9d3bc3be38270f277ea354c234e10398561b
Jacek Wychowałek <rysiuziom1@gmail.com>
2020-12-05 21:57:36 +0100

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Login from "./components/Login";
 import * as Constants from "./Constants";
 import * as Helpers from "./Helpers";
 import BoardView from "./components/Board/BoardView";
-import home_icon from "./assets/home_icon.png";
 import Register from "./components/Register";
+import Navbar from "./components/UI/Navbar";
 
 class App extends React.Component {
   constructor(props) {
@@ -37,11 +37,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="container-fluid mt-5 pt-5">
-        <nav className="navbar navbar-light bg-dark fixed-top">
-          <a className="navbar-brand" href="/">
-            <img src={home_icon} alt="Home page" width="40em" height="40em" />
-          </a>
-        </nav>
+        <Navbar />
         <BrowserRouter>
           <Switch>
             <Route
